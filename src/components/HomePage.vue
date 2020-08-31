809a278dce1f0eedb60c1059c992324af1ce865e
Jensengbg-Leon-Listo <leon.listo@hotmail.com>
2020-08-31 15:42:03 +0200

    first commit
    First commit.

---
+++ src/components/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="container">
+      <nav class="wrapper">
+        <h1 class="heading"> Webshop för iphone skal</h1>
+      </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            items: [
+                {}
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.heading {
+    display: flex;
+    border-bottom: solid;
+    border-color: #FA8585;
+    background-color: #FA8585;
+}
+</style>>
+

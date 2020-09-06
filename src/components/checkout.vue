73f32d17f562650e3eec0191f3c893d252ed6f9d
Jensengbg-Carl-Ulvinen <54705232+Jensengbg-Carl-Ulvinen@users.noreply.github.com>
2020-09-06 10:54:26 +0200

    clean checkout

--- src/components/checkout.vue
+++ src/components/checkout.vue
@@ -1,64 +1,12 @@
 <template>
   <div class="checkout_page">
-      <div class="order_wrapper">
-        <h1>Din beställning</h1>
 
-        <li class="order_list" v-for="items in cart" :key="items.id">
-
-          <div class="title_items">
-            <h2>{{ items.title }}</h2>
-          </div>
-
-          <div class="items_price">
-            <p>{{ items.price * items.quantity }}kr</p>
-            <div class="arrow">
-              <div class="vertical">
-                <img @click="addProd(items)" src="../assets/img/arrow-up.svg" />
-                <h2>{{ items.quantity }}</h2>
-                <img @click="delProd(items)" src="../assets/img/arrow-down.svg" />
-              </div>
-            </div>
-          </div>
-
-        </li>
-
-        <div class="total">
-          <h2 class="total_text">Total</h2>
-        </div>
-
-        <h2 class="total_price">{{ sum }} kr</h2>
-
-        <div class="button">
-
-          <button @click="sendOrder(cart)" class="button_checkout">Beställ!</button>
-        </div>
-      </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: "CheckOut",
-  components: { },
-  data() { },
-  computed: {
-    cart() {
-      return this.$store.state.order.cart;
-    },
-    sum() {
-      let sum = 0;
-      this.cart.forEach(order => (sum += order.totPrice));
-      return sum;
-    }
-  },
-  methods: {
-   addProd(items) {
-      this.$store.commit("addProd", items);
-    },
-    delProd(items) {
-      this.$store.commit("delProd", items);
-    }
-  }
+
 };
 </script>
 
